5086de943c6eb0e8a2b531d419cc5c750d465f1a
Prosper Ugochukwu <ugochukwuchuks222@yahoo.com>
2024-02-22 23:53:19 +0100

    Flask import and initial setup

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 
 """Contains the flask app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/generate_url_qr')
+def generate_url_qr():
+    # Logic for generating QR code for URL
+    return 'QR code for URL generated'
